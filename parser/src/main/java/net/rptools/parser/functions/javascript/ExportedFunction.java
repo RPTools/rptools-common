f21ec1f497cebd31e30f876278a6416d484193d6
Craig Wisniewski <craig.wisniewski@gmail.com>
2013-03-01 22:34:57 +1030

    Updated JavaDoc, checks for null, renamed permission field.

--- parser/src/main/java/net/rptools/parser/functions/javascript/ExportedFunction.java
+++ parser/src/main/java/net/rptools/parser/functions/javascript/ExportedFunction.java
@@ -17,41 +17,108 @@ package net.rptools.parser.functions.javascript;
 import net.rptools.lib.datavalue.DataType;
 import net.rptools.lib.permissions.PermissionLevel;
 
+/**
+ * Represents a function exported from JavaScript code and made available to RPTools script.
+ */
 public class ExportedFunction {
+    /** The exported function name. */
     private final String functionName;
-    private final DataType returnType;
-    private final Object paramList;
-    private final String jsFunctionName;
-    private final PermissionLevel permissionLevel;
 
+    /** The return type of the function. */
+    private final DataType returnType;
+
+    /** The parameter list for the function. */
+    private final Object paramList;
+
+    /** The name of the JavaScript function to call. */
+    private final String jsFunctionName;
+
+    /** The default player permission level required to execute the function. */
+    private final PermissionLevel defaiultPermissionLevel;
+
+    /**
+     * Creates a new ExportedFunction.
+     *
+     * @param name The exported function name.
+     * @param rType The type returned by the function.
+     * @param params The parameter list for the function.
+     * @param jsFuncName The name of the JavaScript function to call.
+     * @param perms The default permission required to call the function.
+     *
+     * @throws  NullPointerException if any of the arguments are null.
+     */
     public ExportedFunction(String name, DataType rType, Object params, String jsFuncName,
                             PermissionLevel perms) {
-        // TODO check for nulls
+        if (name == null) {
+            throw new NullPointerException("Name of exported function is null");
+        }
+
+        if (rType == null) {
+            throw new NullPointerException("Return type of exported function is null");
+        }
+
+        if (params == name) {
+            throw new NullPointerException("Parameter List of exported function is null");
+        }
+
+        if (jsFuncName == name) {
+            throw new NullPointerException("JavaScript name of exported function is null");
+        }
+
+        if (perms == null) {
+            throw new NullPointerException("Default permission level for function is null");
+        }
+
         functionName = name;
         returnType = rType;
         paramList = params;
         jsFunctionName = jsFuncName;
-        permissionLevel = perms;
+        defaiultPermissionLevel = perms;
     }
 
 
+    /**
+     * Returns the exported name of the function.
+     *
+     * @return the exported name.
+     */
     public String getFunctionName() {
         return functionName;
     }
 
+    /**
+     * Returns the return type of the function.
+     *
+     * @return the return type.
+     */
     public DataType getReturnType() {
         return returnType;
     }
 
+    /**
+     * Returns the parameter list of the function.
+     *
+     * @return the parameter list.
+     */
     public Object getParamList() {
         return paramList;
     }
 
+    /**
+     * Returns the JavaScript function name to call.
+     *
+     * @return the JavaSCript function name.
+     */
     public String getJsFunctionName() {
         return jsFunctionName;
     }
 
-    public PermissionLevel getPermissionLevel() {
-        return permissionLevel;
+    /**
+     * Gets the default permission level required to call the function.
+     *
+     * @return the default permission level.
+     */
+    public PermissionLevel getDefaultPermissionLevel() {
+        return defaiultPermissionLevel;
     }
 }
